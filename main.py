7c6dd4b2979e28937f4537e1100e808bd03a0663
Penut <Liu.ev@ufl.edu>
2023-03-09 18:16:09 -0500

    Encode

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+copassword = ""
+#menu
+print("Menu")
+print("------------- ")
+print("1. Encode ")
+print("2. Decode")
+print("3. Quit ")
+print("")
 
-print("tesing")
+
+#options
+choice = int(input("Please enter an option: "))
+
+if choice == 1:
+    #encode
+    password = input("Please enter your password to encode: ")
+    holdlist = []
+    holdlist[:0] = password
+    for i in range(len(password)):
+        for j in range(5):
+            holdlist[i] = int(holdlist[i]) + 1
+            if int(holdlist[i]) == 10:
+                holdlist[i] = 0
+    for num in holdlist:
+        copassword += str(num)
+elif choice == 2:
+    pass
