c58ede5eb485fb3c559f4bb6c98d9317db921f64
Penut <Liu.ev@ufl.edu>
2023-03-09 18:26:39 -0500

    Encoded Done

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ print("")
 
 #options
 choice = int(input("Please enter an option: "))
-
-if choice == 1:
-    #encode
-    password = input("Please enter your password to encode: ")
-    holdlist = []
-    holdlist[:0] = password
-    for i in range(len(password)):
-        for j in range(5):
-            holdlist[i] = int(holdlist[i]) + 1
-            if int(holdlist[i]) == 10:
-                holdlist[i] = 0
-    #turn back to string
-    for num in holdlist:
-        copassword += str(num)
-    print("Your password has been encoded and stored! ")
-elif choice == 2:
-    pass
-elif choice == 3:
-    exit()
+while True:
+    if choice == 1:
+        #encode
+        password = input("Please enter your password to encode: ")
+        holdlist = []
+        holdlist[:0] = password
+        for i in range(len(password)):
+            for j in range(5):
+                holdlist[i] = int(holdlist[i]) + 1
+                if int(holdlist[i]) == 10:
+                    holdlist[i] = 0
+        #turn back to string
+        for num in holdlist:
+            copassword += str(num)
+        print("Your password has been encoded and stored! ")
+        print()
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        exit()
